33011d66031caa1c46d9be6331949bed7b2e646c
peterson <jooxpeters@gmail.com>
2022-02-15 20:59:14 -0300

    working

--- main.py
+++ main.py
@@ -3,23 +3,15 @@ import numpy as np
 import imutils
 
 def main():
-    real = cv.imread("images/pcb-real-3.png", cv.IMREAD_COLOR)
-    cad = cv.imread("images/pcb-cad-3.png", cv.IMREAD_GRAYSCALE)
+    orig = cv.imread("images/pcb-1-fail.png", cv.IMREAD_COLOR)
+    cad = cv.imread("images/pcb-1-mask.png", cv.IMREAD_GRAYSCALE)
 
-    cad = cv.resize(cad, (real.shape[1], real.shape[0]))
+    cad = cv.resize(cad, (orig.shape[1], orig.shape[0]))
 
-    real = cv.GaussianBlur(real, (11,11), 0)
-    hsv = cv.cvtColor(real, cv.COLOR_BGR2HSV)
-
-    # mask1 = cv.inRange(hsv, (170, 38, 38), (180, 153, 255))
-    mask2 = cv.inRange(hsv, (8,  20, 10), (40,  255,  127))
-    mask2 = cv.bitwise_not(mask2)
-
-    cv.imshow("mask", mask2)
-    cv.waitKey()
+    real = cv.GaussianBlur(orig, (11,11), 0)
+    cad = cv.GaussianBlur(cad, (11,11), 0)
 
     real = cv.cvtColor(real, cv.COLOR_BGR2GRAY)
-    # real = cv.bitwise_and(real, mask2)
 
     # (_, real) = cv.threshold(real, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
     (_, cad) = cv.threshold(cad, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
@@ -27,15 +19,26 @@ def main():
     # cad = cv.adaptiveThreshold(cad, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 51, 0)
 
     diff = cv.bitwise_xor(real, cad)
+    diff = cv.GaussianBlur(diff, (31,31), 0)
+    (_, diff) = cv.threshold(diff, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
 
-    cv.imshow("real", real)
-    cv.imshow("cad", cad)
+    # cont = cv.findContours(diff, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # cont = imutils.grab_contours(cont)
+    # for c in cont:
+    #     (x, y, w, h) = cv.boundingRect(c)
+    #     cv.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    cont, hierarchy = cv.findContours(diff, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    for i in range(len(cont)):
+        cv.drawContours(orig, cont, i, (0,0,255), 2, cv.LINE_8, hierarchy, 0)
+
+
+    cv.imshow("orig", orig)
+    # cv.imshow("real", real)
+    # cv.imshow("cad", cad)
     cv.imshow("diff", diff)
     cv.waitKey()
 
-    # real = cv.findContours(real, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    # real = imutils.grab_contours(real)
-    
 
 if __name__ == "__main__":
     main()
